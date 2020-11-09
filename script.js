fa3d203913a771aec8f18cb6688669326fbd335d
Cassandre <cassandre.gaudinat@gmail.com>
2020-11-09 18:52:28 +0000

    all

---
+++ script.js
@@ -0,0 +1,33 @@
+var firstName= "Cassandre";
+var  favouriteColor= "Blue";
+
+var favouriteFood ="Spanish food"
+var city ="Paris"
+
+// This following code will display in the terminal of the browser, the values of each variable
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood);
+console.log(city);
+
+
+var phrase= "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all. ";
+var endingPhrase= " is what we see. Rainbow colours, are made with light bending you see!";
+var one= "Red ";
+var two= "orange ";
+var three= "yellow ";
+var four="green ";
+var five="blue ";
+var six="indigo ";
+var seven="violet";
+
+
+console.log(phrase + one + two + three + four + five + six + seven + endingPhrase);
+
+
+var myAlphabet=["A","B","C","D","E","F","G"]
+console.log(myAlphabet.length)
+
+var myAlphabetLength=7;
+console.log(myAlphabetLength);
+
